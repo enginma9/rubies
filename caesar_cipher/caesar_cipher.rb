d08d66c6602316c9d52731d1d9ccbd82092f54be
enginma9 <106038775+enginma9@users.noreply.github.com>
2023-09-03 20:17:56 -0500

    caesar_cipher passing rSpec

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,6 +1,26 @@
-def caesar_cypher(string, shift){
+def rotate( code, shift ) #after bringing value of A or a to 0, add the shift value, get modulus 26, then recombine
+    ( code + shift ) % 26
+end
 
-}
-
-caesar_cipher("What a string!", 5)
-"Bmfy f xywnsl!"
+def caesar_cypher( string, shift )
+    result_string = ""
+    #65.chr A.ord
+    #iterate over all characters in the string and get char_code, 
+    #find whether in range for being a letter, and whether upcase/downcase, 65(A)-90(Z) 97(a)-122(z)
+    
+    string.each_char do |char| 
+        code = char.ord
+        #p code
+        if code.between?( 65, 90 )
+            #puts "UPCASE"
+            code = rotate( code - 65, shift) + 65
+        elsif code.between?( 97, 122 )
+            #puts "downcase"
+            code = rotate( code - 97, shift) + 97
+        else
+            #puts "nope"
+        end
+        result_string += code.chr
+    end
+    return result_string
+end
--- caesar_cipher/spec/caesar_cipher_spec.rb
+++ caesar_cipher/spec/caesar_cipher_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 require_relative '../caesar_cipher'
 
-RSpec.describe "test caesar cipher" do 
-   describe 'test' do
-      it "caesar_cipher(\"What a string!\", 5) should return \"Bmfy f xywnsl!\"" do
-         expect(caesar_cipher("What a string!")).to_eq("Bmfy f xywnsl!")
-      end
+RSpec.describe "test caesar cipher\n" do 
+   it "caesar_cipher(\"What a string!\", 5) should return \"Bmfy f xywnsl!\"" do
+      expect( caesar_cypher( "What a string!", 5 ) ).to eq( "Bmfy f xywnsl!" )
    end
 end
 
